1d56500bb617e1f91315bb0e0ff715d9f7a44eec
sairoutine <sairoutine@gmail.com>
2017-06-01 22:40:39 +0900

    update: main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,43 @@
+'use strict';
+var Game = require('./game');
 
+var game;
+
+window.onload = function() {
+	// Canvas
+	var mainCanvas = document.getElementById('mainCanvas');
+	// Game オブジェクト
+	game = new Game(mainCanvas, {
+		webgl: true,
+	});
+
+	// イベントハンドラの設定
+	game.setupEvents();
+
+	// 初期化
+	game.init();
+
+	// ゲーム起動
+	game.startRun();
+};
+window.onerror = function (msg, file, line, column, err) {
+	/*
+	msg: error message
+	file: file path
+	line: row number
+	column: column number
+	err: error object
+	*/ 
+	//window.alert(msg + "\n" + line + ":" + column);
+};
+/*
+window.runGame = function () {
+	game.startRun();
+};
+window.stopGame = function () {
+	game.stopRun();
+};
+*/
+window.changeFullScreen = function () {
+	game.fullscreen();
+};
