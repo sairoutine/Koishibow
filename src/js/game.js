1062c95cbc3784e99a2d79275a10d9de216ea399
sairoutine <sairoutine@gmail.com>
2017-06-01 22:40:52 +0900

    add: game class

---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+'use strict';
+var core = require('./hakurei').core;
+var util = require('./hakurei').util;
+
+var Game = function(canvas) {
+	core.apply(this, arguments);
+};
+util.inherit(Game, core);
+
+Game.prototype.init = function () {
+	core.prototype.init.apply(this, arguments);
+
+	//this.addScene("stage", new SceneStage(this));
+
+	//this.changeScene("loading");
+};
+module.exports = Game;
