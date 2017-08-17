9caf1e495c463cc3a3b42501dc7c80fdb393abfc
sairoutine <sairoutine@gmail.com>
2017-08-18 01:36:04 +0900

    update: stage.js

--- src/js/scene/stage.js
+++ src/js/scene/stage.js
@@ -8,10 +8,23 @@ var Koishi = require('../object/koishi');
 var SceneStage = function(core) {
 	base_scene.apply(this, arguments);
 
-	this.koishi = new Koishi(this);
-	this.koishi.init(180, 360);
 };
 util.inherit(SceneStage, base_scene);
+
+SceneStage.prototype.init = function(is_left){
+	base_scene.prototype.init.apply(this, arguments);
+
+	this.setFadeIn(30, "black");
+
+	this.koishi = new Koishi(this);
+	if (is_left) {
+		this.koishi.init(this.width - 180, 360);
+		this.koishi.sprite.setReflect(true);
+	}
+	else {
+		this.koishi.init(180, 360);
+	}
+};
 
 SceneStage.prototype.beforeDraw = function(){
 	base_scene.prototype.beforeDraw.apply(this, arguments);
@@ -21,7 +34,24 @@ SceneStage.prototype.beforeDraw = function(){
 		var x = this.core.input_manager.mousePositionX();
 		var y = this.core.input_manager.mousePositionY();
 
-		this.koishi.setMoveTarget(x, y);
+		var collision_size = 30;
+		// TODO: ちゃんと書く
+		if (0+24 - collision_size/2 < x && x < 0+24 + collision_size/2 &&
+			this.height/2 - collision_size/2 < y && y < this.height/2 + collision_size) {
+			// フィールド遷移
+			this.setFadeOut(30, "black");
+			this.core.changeScene("stage", true);
+		}
+		else if (this.width - 24 - collision_size/2 < x && x < this.width - 24 + collision_size/2 &&
+			this.height/2 - collision_size/2 < y && y < this.height/2 + collision_size) {
+			// フィールド遷移
+			this.setFadeOut(30, "black");
+			this.core.changeScene("stage");
+		}
+		else {
+			// こいしを移動
+			this.koishi.setMoveTarget(x, y);
+		}
 	}
 
 	this.koishi.beforeDraw();
@@ -32,7 +62,7 @@ SceneStage.prototype.draw = function(){
 	this.core.clearCanvas();
 	var ctx = this.core.ctx;
 
-
+	// 背景描画
 	var title_bg = this.core.image_loader.getImage('bg');
 	ctx.save();
 	ctx.drawImage(title_bg,
@@ -45,6 +75,21 @@ SceneStage.prototype.draw = function(){
 					this.core.width,
 					this.core.height);
 	ctx.restore();
+
+	// フィールド遷移矢印
+	ctx.font = "48px 'Migu'";
+	ctx.textAlign = 'center';
+	ctx.textBaseAlign = 'middle';
+	ctx.fillStyle = 'rgb( 255, 255, 255 )';
+	ctx.fillText("◀", 0 + 24, this.height/2);
+	ctx.fillText("▶", this.width - 24, this.height/2);
+
+
+
+
+
+
+	// こいし表示
 	this.koishi.draw();
 };
 
