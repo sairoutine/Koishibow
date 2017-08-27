bdca7254c2ca1a67b34f1aa04127f4e722957daa
sairoutine <sairoutine@gmail.com>
2017-08-28 00:51:29 +0900

    add: sub_stage play and talk

---
+++ src/js/scene/sub_stage/play.js
@@ -0,0 +1,9 @@
+'use strict';
+var base_scene = require('./base');
+var Util = require('../hakurei').util;
+var SceneSubStagePlay = function(core) {
+	base_scene.apply(this, arguments);
+};
+Util.inherit(SceneSubStagePlay, base_scene);
+
+module.exports = SceneSubStagePlay;
---
+++ src/js/scene/sub_stage/talk.js
@@ -0,0 +1,9 @@
+'use strict';
+var base_scene = require('./base');
+var Util = require('../hakurei').util;
+var SceneSubStageTalk = function(core) {
+	base_scene.apply(this, arguments);
+};
+Util.inherit(SceneSubStageTalk, base_scene);
+
+module.exports = SceneSubStageTalk;
