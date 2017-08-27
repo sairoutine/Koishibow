b72e39d68c661be1a2724b5957a7166bb58d8dbf
sairoutine <sairoutine@gmail.com>
2017-08-28 01:56:59 +0900

    update: refactor

--- src/js/scene/stage.js
+++ src/js/scene/stage.js
@@ -55,49 +55,6 @@ SceneStage.prototype.init = function(is_left){
 
 SceneStage.prototype.beforeDraw = function(){
 	base_scene.prototype.beforeDraw.apply(this, arguments);
-
-	if(this.core.input_manager.isLeftClickPush()) {
-		// 左クリックしたところを取得
-		var x = this.core.input_manager.mousePositionX();
-		var y = this.core.input_manager.mousePositionY();
-
-		// TODO: サブシーンにまとめたい...
-
-		var collision_size = 1000;
-
-		// 会話するオブジェクト
-		// TODO: オブジェクトをクリックし続けないと画面遷移しないのを、画面全体クリックして遷移するようにしないと
-		var talk_width  = 100;
-		var talk_height = 100;
-		// play sub scene へ移植 TODO:
-		if (talk_width - 24 - collision_size/2 < x && x < talk_width - 24 + collision_size/2 &&
-				talk_height/2 - collision_size/2 < y && y < talk_height/2 + collision_size) {
-			if (!(this.currentSubScene() instanceof SceneSubStageTalk)) {
-				this.changeSubScene("talk");
-			}
-		}
-		// それ以外
-		else {
-			collision_size = 30;
-			// TODO: ちゃんと書く
-			if (0+24 - collision_size/2 < x && x < 0+24 + collision_size/2 &&
-				this.height/2 - collision_size/2 < y && y < this.height/2 + collision_size) {
-				// フィールド遷移
-				this.setFadeOut(30, "black");
-				this.core.changeScene("stage", true);
-			}
-			else if (this.width - 24 - collision_size/2 < x && x < this.width - 24 + collision_size/2 &&
-				this.height/2 - collision_size/2 < y && y < this.height/2 + collision_size) {
-				// フィールド遷移
-				this.setFadeOut(30, "black");
-				this.core.changeScene("stage");
-			}
-			else {
-				// こいしを移動
-				this.koishi().setMoveTarget(x, y);
-			}
-		}
-	}
 };
 
 // 画面更新
--- src/js/scene/sub_stage/play.js
+++ src/js/scene/sub_stage/play.js
@@ -20,9 +20,50 @@ SceneSubStagePlay.prototype.draw = function(){
 	ctx.fillText("▶", this.width - 24, this.height/2);
 };
 
+SceneSubStagePlay.prototype.beforeDraw = function(){
+	base_scene.prototype.beforeDraw.apply(this, arguments);
 
+	if(this.core.input_manager.isLeftClickPush()) {
+		// 左クリックしたところを取得
+		var x = this.core.input_manager.mousePositionX();
+		var y = this.core.input_manager.mousePositionY();
 
+		// TODO: サブシーンにまとめたい...
 
+		var collision_size = 1000;
+
+		// 会話するオブジェクト
+		// TODO: オブジェクトをクリックし続けないと画面遷移しないのを、画面全体クリックして遷移するようにしないと
+		var talk_width  = 100;
+		var talk_height = 100;
+		// play sub scene へ移植 TODO:
+		if (talk_width - 24 - collision_size/2 < x && x < talk_width - 24 + collision_size/2 &&
+				talk_height/2 - collision_size/2 < y && y < talk_height/2 + collision_size) {
+			this.mainStage().changeSubScene("talk");
+		}
+		// それ以外
+		else {
+			collision_size = 30;
+			// TODO: ちゃんと書く
+			if (0+24 - collision_size/2 < x && x < 0+24 + collision_size/2 &&
+				this.mainStage().height/2 - collision_size/2 < y && y < this.mainStage().height/2 + collision_size) {
+				// フィールド遷移
+				this.mainStage().setFadeOut(30, "black");
+				this.core.changeScene("stage", true);
+			}
+			else if (this.mainStage().width - 24 - collision_size/2 < x && x < this.mainStage().width - 24 + collision_size/2 &&
+				this.mainStage().height/2 - collision_size/2 < y && y < this.mainStage().height/2 + collision_size) {
+				// フィールド遷移
+				this.mainStage().setFadeOut(30, "black");
+				this.core.changeScene("stage");
+			}
+			else {
+				// こいしを移動
+				this.koishi().setMoveTarget(x, y);
+			}
+		}
+	}
+};
 
 
 module.exports = SceneSubStagePlay;
