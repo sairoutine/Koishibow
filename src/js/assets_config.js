e6a4a50bcb9a8b7efb027841485d7a698bb01a94
sairoutine <sairoutine@gmail.com>
2017-09-23 18:59:32 +0900

    update: assets

--- src/js/assets_config.js
+++ src/js/assets_config.js
@@ -2,7 +2,7 @@
 
 var AssetsConfig = {};
 AssetsConfig.images = {
-	bg:  "./image/bg.jpg",
+	bg:  "./image/bg.png",
 	fukidashi:  "./image/fukidashi.png",
 	title:  "./image/title.png",
 	logo_wht:  "./image/logo_wht.png",
