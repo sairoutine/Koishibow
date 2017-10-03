76668a1871a8555ab882ae07b95e9a6ec7ed954b
sairoutine <sairoutine@gmail.com>
2017-10-03 12:21:24 +0900

    add: chapter0-myroom static object and bg

--- src/js/assets_config.js
+++ src/js/assets_config.js
@@ -2,7 +2,13 @@
 
 var AssetsConfig = {};
 AssetsConfig.images = {
+	"chapter0-myroom-bg-001":    "./image/chapter0/myroom/chapter0-myroom-bg-001.jpg",
+	"chapter0-myroom-obj-01-01": "./image/chapter0/myroom/chapter0-myroom-obj-01-01.png",
+	"chapter0-myroom-obj-05-01": "./image/chapter0/myroom/chapter0-myroom-obj-05-01.png",
+
+	// TODO: delete
 	bg:  "./image/bg.png",
+
 	fukidashi:  "./image/fukidashi.png",
 	title:  "./image/title.png",
 	logo_wht:  "./image/logo_wht.png",
