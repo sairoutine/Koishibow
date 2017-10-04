f63c66fbe822fb4254329567f61b195d9fc52fd7
sairoutine <sairoutine@gmail.com>
2017-10-04 14:49:08 +0900

    update: hospital_corridor1 objects

--- src/js/field/chapter0/hospital_corridor1.js
+++ src/js/field/chapter0/hospital_corridor1.js
@@ -6,7 +6,11 @@ module.exports = {
 	left_start_position:  {x: 180, y: 540},
 	right_field: "chapter0_hospital_corridor2",
 	left_field: "chapter0_myroom",
-	background: "bg",
+	background: "chapter0-hospital_corridor1-bg-001",
 	objects: [
+		{image: "chapter0-hospital_corridor1-obj-01-01", name: "手術台", x: 555, y: 470, scale: 0.7},
+		{image: "chapter0-hospital_corridor1-obj-02-01", name: "金庫",   x: 205, y: 300, scale: 0.7},
+		{image: "chapter0-hospital_corridor1-obj-03-01", name: "バケツ", x: 230, y: 240, scale: 0.7},
+		{image: "chapter0-hospital_corridor1-obj-04-01", name: "メス",   x: 830, y: 530, scale: 0.7},
 	],
 };
