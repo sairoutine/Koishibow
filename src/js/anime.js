a364c6e32435b4e8ba49d9da02c54a5164f3b01c
sairoutine <sairoutine@gmail.com>
2017-10-06 18:02:05 +0900

    update: add anime of obj-03~05

--- src/js/anime.js
+++ src/js/anime.js
@@ -1,17 +1,16 @@
-var anime_list = [
-	require('./anime/chapter0/myroom/obj-02-01/obj01_anime_1'),
-	require('./anime/chapter0/myroom/obj-02-01/obj02_anime_1'),
-	require('./anime/chapter0/myroom/obj-02-01/obj03_anime_1'),
-];
+'use strict';
+var anime_map = {
+	"chapter0-myroom-obj-02-01-obj01": require('./anime/chapter0/myroom/obj-02-01/obj01_anime_1'),
+	"chapter0-myroom-obj-02-01-obj02": require('./anime/chapter0/myroom/obj-02-01/obj02_anime_1'),
+	"chapter0-myroom-obj-02-01-obj03": require('./anime/chapter0/myroom/obj-02-01/obj03_anime_1'),
+	"chapter0-myroom-obj-03-01-obj01": require('./anime/chapter0/myroom/obj-03-01/obj01_anime_1'),
+	"chapter0-myroom-obj-03-01-obj02": require('./anime/chapter0/myroom/obj-03-01/obj02_anime_1'),
+	"chapter0-myroom-obj-03-01-obj03": require('./anime/chapter0/myroom/obj-03-01/obj03_anime_1'),
+	"chapter0-myroom-obj-04-01-obj01": require('./anime/chapter0/myroom/obj-04-01/obj01_anime_1'),
+	"chapter0-myroom-obj-04-01-obj02": require('./anime/chapter0/myroom/obj-04-01/obj02_anime_1'),
+	"chapter0-myroom-obj-04-01-obj03": require('./anime/chapter0/myroom/obj-04-01/obj03_anime_1'),
 
-var anime_map = {};
+};
 
-for (var i = 0, len = anime_list.length; i < len; i++) {
-	var anime = anime_list[i];
-	// index = 0 のみ有効
-	anime_map[anime[0].name] = anime;
-
-	// TODO: key 名が被ってたらデバッグではエラー
-}
 module.exports = anime_map;
 
