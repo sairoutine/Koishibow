3afe0d9166271b25e02a15e87593f7d46e4043f0
sairoutine <sairoutine@gmail.com>
2017-10-06 18:02:43 +0900

    update: myroom data

--- src/js/field/chapter0/myroom.js
+++ src/js/field/chapter0/myroom.js
@@ -11,8 +11,9 @@ module.exports = {
 	objects: [
 		{image: "chapter0-myroom-obj-01-01", type: CONSTANT.STATIC_IMAGE_TYPE, name: "ベッド", x: 80, y: 270, scale: 0.3},
 		{image: "chapter0-myroom-obj-05-01", type: CONSTANT.STATIC_IMAGE_TYPE, name: "机", x: 0, y: 425, scale: 0.3},
-		{image: "chapter0-myroom-obj-02-01", type: CONSTANT.ANIME_IMAGE_TYPE,  name: "本", x: 340, y: 530, scale: 0.3, anime1: "obj01_anime_1", anime2: "obj02_anime_1", anime3: "obj03_anime_1",
-		}
+		{image: "chapter0-myroom-obj-02-01", type: CONSTANT.ANIME_IMAGE_TYPE,  name: "本", x: 340, y: 530, scale: 0.3, anime1: "chapter0-myroom-obj-02-01-obj01", anime2: "chapter0-myroom-obj-02-01-obj02", anime3: "chapter0-myroom-obj-02-01-obj03"},
+		{image: "chapter0-myroom-obj-03-01", type: CONSTANT.ANIME_IMAGE_TYPE,  name: "クレヨン", x: 600, y: 530, scale: 0.3, anime1: "chapter0-myroom-obj-03-01-obj01", anime2: "chapter0-myroom-obj-03-01-obj02", anime3: "chapter0-myroom-obj-03-01-obj03"},
+		{image: "chapter0-myroom-obj-04-01", type: CONSTANT.ANIME_IMAGE_TYPE,  name: "まど", x: 5, y: 180, scale: 0.3, anime1: "chapter0-myroom-obj-04-01-obj01", anime2: "chapter0-myroom-obj-04-01-obj02", anime3: "chapter0-myroom-obj-04-01-obj03"},
 	],
 };
 
