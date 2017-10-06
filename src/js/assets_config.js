42c3a0fa83b9c04461b24cebef2a579197ccd774
sairoutine <sairoutine@gmail.com>
2017-10-07 02:02:08 +0900

    update: rename obj images path

--- src/js/assets_config.js
+++ src/js/assets_config.js
@@ -5,9 +5,9 @@ AssetsConfig.images = {
 	"koishi.png":                "./image/koishi.png",
 	"chapter0-myroom-bg-001":    "./image/chapter0/myroom/chapter0-myroom-bg-001.jpg",
 	"chapter0-myroom-obj-01-01": "./image/chapter0/myroom/chapter0-myroom-obj-01-01.png",
-	"chapter0-myroom-obj-02-01.png": "./image/chapter0-myroom-obj-02-01.png",
-	"chapter0-myroom-obj-03-01.png": "./image/chapter0-myroom-obj-03-01.png",
-	"chapter0-myroom-obj-04-01.png": "./image/chapter0-myroom-obj-04-01.png",
+	"chapter0-myroom-obj-02-01.png": "./image/chapter0/myroom/chapter0-myroom-obj-02-01.png",
+	"chapter0-myroom-obj-03-01.png": "./image/chapter0/myroom/chapter0-myroom-obj-03-01.png",
+	"chapter0-myroom-obj-04-01.png": "./image/chapter0/myroom/chapter0-myroom-obj-04-01.png",
 	"chapter0-myroom-obj-05-01": "./image/chapter0/myroom/chapter0-myroom-obj-05-01.png",
 
 	"chapter0-hospital_corridor1-bg-001":    "./image/chapter0/hospital_corridor1/chapter0-hospital-bg-001.jpg",
