98902409e6b39bb552d0fd711d063efc4623735b
sairoutine <sairoutine@gmail.com>
2017-11-14 12:40:22 +0900

    update: debug_constant.js

--- src/js/debug_constant.js
+++ src/js/debug_constant.js
@@ -3,7 +3,7 @@ var DEBUG = {
 	ON: true,
 	SOUND_OFF: false,
 	// 第一引数: scene name, 第二引数以降: 引数
-	START_SCENE: ["stage", "chapter0_hospital_corridor1"],
+	//START_SCENE: ["stage", "chapter0_mansion_corridor3"],
 };
 
 module.exports = DEBUG;
