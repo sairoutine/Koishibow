50af2db9fe15055c99b49aedab96f8ad7ba8fcc2
sairoutine <sairoutine@gmail.com>
2017-12-09 18:08:30 +0900

    add: mock of sub scenes

---
+++ src/js/scene/subscene/base.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var base_scene = require('../../../hakurei').scene.base;
+
+var Util = require('../../../hakurei').util;
+
+var SceneSceneSubStageBase = function(core) {
+	base_scene.apply(this, arguments);
+
+};
+Util.inherit(SceneSceneSubStageBase, base_scene);
+
+module.exports = SceneSceneSubStageBase;
---
+++ src/js/scene/subscene/play.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var base_scene = require('./base');
+
+var Util = require('../../../hakurei').util;
+
+var SceneDefault = function(core) {
+	base_scene.apply(this, arguments);
+
+};
+Util.inherit(SceneDefault, base_scene);
+
+module.exports = SceneDefault;
