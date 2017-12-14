d9bbd1068746031ee4854885b4a1aa7a817a771b
sairoutine <sairoutine@gmail.com>
2017-12-15 03:49:36 +0900

    update: switch off item menu in showing item menu

--- src/js/scene/substage/menu.js
+++ src/js/scene/substage/menu.js
@@ -10,4 +10,21 @@ var SceneDefault = function(core) {
 };
 Util.inherit(SceneDefault, base_scene);
 
+SceneDefault.prototype.beforeDraw = function(){
+	base_scene.prototype.beforeDraw.apply(this, arguments);
+
+	// マウス座標取得
+	var x = this.core.input_manager.mousePositionX();
+	var y = this.core.input_manager.mousePositionY();
+
+	// アイテムメニューを閉じる
+	if(this.parent.root().item_menu_button.checkCollisionWithPosition(x, y)) {
+		return true;
+	}
+
+};
+
+
+
+
 module.exports = SceneDefault;
