a8e25a929a4adcb23fd79b0c0c563751fe5bdbec
sairoutine <sairoutine@gmail.com>
2017-12-15 03:50:25 +0900

    re-implement: substage play class

--- src/js/scene/substage/play.js
+++ src/js/scene/substage/play.js
@@ -4,10 +4,94 @@ var base_scene = require('./base');
 
 var Util = require('../../hakurei').util;
 
-var SceneDefault = function(core) {
+var SceneSubStagePlay = function(core) {
 	base_scene.apply(this, arguments);
 
 };
-Util.inherit(SceneDefault, base_scene);
+Util.inherit(SceneSubStagePlay, base_scene);
 
-module.exports = SceneDefault;
+SceneSubStagePlay.prototype.init = function(){
+	base_scene.prototype.init.apply(this, arguments);
+};
+
+SceneSubStagePlay.prototype.beforeDraw = function(){
+	base_scene.prototype.beforeDraw.apply(this, arguments);
+
+	// サードアイの光の当たり判定
+	this._collisionLightCheck();
+
+	// 当たり判定チェック
+	this._collisionCheck();
+};
+
+// 当たり判定チェック
+SceneSubStagePlay.prototype._collisionCheck = function(){
+	// マウス座標取得
+	var x = this.core.input_manager.mousePositionX();
+	var y = this.core.input_manager.mousePositionY();
+
+	// シーン遷移
+	if(this.root().right_next_field_button.checkCollisionWithPosition(x, y)) {
+		return true;
+	}
+	// シーン遷移
+	else if(this.root().left_next_field_button.checkCollisionWithPosition(x, y)) {
+		return true;
+	}
+	// アイテムメニュー
+	else if(this.root().item_menu_button.checkCollisionWithPosition(x, y)) {
+		return true;
+	}
+	// サードアイ使用
+	else if(this.root().eye_button.checkCollisionWithPosition(x, y)) {
+		return true;
+	}
+
+	// フィールドの各種オブジェクトとの当たり判定
+	for (var i = this.root().pieces.length - 1; i >= 0; i--) { // i の大きい方が手前なので
+		var piece = this.root().pieces[i];
+		if(piece.checkCollisionWithPosition(x, y)) {
+			return true;
+		}
+	}
+
+	// UI と 画面上のオブジェクトの どれとも当たり判定しなかったら
+	if(this.core.input_manager.isLeftClickPush()) {
+		// 移動中でなければ
+		if (!this.root().koishi.isMoving()) {
+			var point = this.core.input_manager.mousePositionPoint(this.root());
+			// こいしを移動
+			this.root().koishi.setMoveTarget(point);
+		}
+	}
+};
+SceneSubStagePlay.prototype._collisionLightCheck = function(){
+	/*
+	// 3rd eye の光の当たり判定チェック
+
+	if (this.mainStage().isUsingEye()) {
+		// フィールドの各種オブジェクトとの当たり判定
+		for (var i = 0, len = this.mainStage().pieces.length; i < len; i++) {
+			var piece = this.mainStage().pieces[i];
+
+			if(piece instanceof ObjectAnimeImage) {
+				if (LogicCollideWithLight.check(piece, this.mainStage())) {
+					piece.onMouseOver();
+				}
+			}
+		}
+	}
+	*/
+
+	return false;
+};
+
+SceneSubStagePlay.prototype.draw = function(){
+	base_scene.prototype.draw.apply(this, arguments);
+};
+
+SceneSubStagePlay.prototype.afterDraw = function(){
+	base_scene.prototype.afterDraw.apply(this, arguments);
+};
+
+module.exports = SceneSubStagePlay;
