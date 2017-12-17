285369861ce2ff24d14b87ee946a63fdfc7f48be
sairoutine <sairoutine@gmail.com>
2017-12-18 00:59:38 +0900

    add: config item

---
+++ src/js/config/item.js
@@ -0,0 +1,15 @@
+'use strict';
+var CONSTANT = require('../constant');
+
+var Item = {};
+
+Item[CONSTANT.ITEM.EYEDROP] = {
+	name: "目薬",
+	image_name: "item_01",
+	description: "使うと目の充血がやわらぐ",
+};
+
+
+
+module.exports = Item;
+
