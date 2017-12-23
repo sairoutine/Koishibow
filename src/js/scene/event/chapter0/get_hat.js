0d8f76eed154e4ad57266207db5ac6b121614d19
sairoutine <sairoutine@gmail.com>
2017-12-23 20:07:29 +0900

    update: get_hat event

--- src/js/scene/event/chapter0/get_hat.js
+++ src/js/scene/event/chapter0/get_hat.js
@@ -16,11 +16,18 @@ var SceneEventChapter0GetHat = function(core) {
 	// 帽子獲得 1枚絵
 	this.addSubScene("picture_get_hat", new SceneSubStageGetHat(core));
 
+	// カーソルの状態
+	this._cursor_y = 0;
+	this._cursor_reverse = false;
 };
 Util.inherit(SceneEventChapter0GetHat, base_scene);
 
 SceneEventChapter0GetHat.prototype.init = function(){
 	base_scene.prototype.init.apply(this, ["chapter0_myroom"]);
+
+	// カーソルの状態
+	this._cursor_y = 0;
+	this._cursor_reverse = false;
 };
 
 SceneEventChapter0GetHat.prototype.beforeDraw = function() {
@@ -33,7 +40,7 @@ SceneEventChapter0GetHat.prototype.draw = function(){
 	var ctx = this.core.ctx;
 
 	// 背景描画
-	var bg = this.core.image_loader.getImage("get_hat_bg");
+	var bg = this.core.image_loader.getImage(field_data.background);
 	ctx.save();
 	ctx.drawImage(bg,
 		0,
@@ -52,6 +59,35 @@ SceneEventChapter0GetHat.prototype.draw = function(){
 		this.pieces[i].draw();
 	}
 
+	// カーソルを上下に移動させる
+	if (this._cursor_reverse) {
+		this._cursor_y-=0.25;
+	}
+	else {
+		this._cursor_y+=0.25;
+	}
+
+	if (this._cursor_y > 6) {
+		this._cursor_reverse = true;
+	}
+	else if (this._cursor_y === 0) {
+		this._cursor_reverse = false;
+	}
+
+	// 黒い矢印
+	var cursor = this.core.image_loader.getImage("target_arrow");
+	ctx.save();
+	ctx.drawImage(cursor,
+		747, // 18 -> font size
+		479 + this._cursor_y, // TODO: 要調整
+		cursor.width*2/3,
+		cursor.height*2/3
+	);
+	ctx.restore();
+
+
+
+
 	// 自機
 	this.koishi.draw();
 
@@ -66,7 +102,7 @@ SceneEventChapter0GetHat.prototype.draw = function(){
 };
 
 SceneEventChapter0GetHat.prototype._setupPieces = function() {
-	this.pieces = [];
+	base_scene.prototype._setupPieces.apply(this, arguments);
 
 	var object = new ObjectChapter0Hat(this);
 	object.init();
