63565ed3177228d6ef327c41e9c30a5c10d18702
sairoutine <sairoutine@gmail.com>
2018-01-24 05:32:31 +0900

    update: delete showing serif methods in talk_with_object class because of moving methods to object

--- src/js/scene/substage/talk_with_object.js
+++ src/js/scene/substage/talk_with_object.js
@@ -77,74 +77,11 @@ SceneSubStageObjectTalk.prototype.afterDraw = function(){
 
 	// セリフ送り待機中は表示しない
 	if(!this._next_to_serif_waiting_count) {
-		// メッセージウィンドウ表示
-		this._showMessageWindow();
-
-		// メッセージ表示
+		// セリフ表示
 		this._showMessage();
-	}
-};
 
-// セリフウィンドウ表示
-SceneSubStageObjectTalk.prototype._showMessageWindow = function(){
-	var ctx = this.core.ctx;
-	ctx.save();
-
-	var fukidashi = this.core.image_loader.getImage('fukidashi');
-
-	// ウィンドウの位置
-	var message_window_pos = this._getMessageWindowPos();
-	var x = message_window_pos.x;
-	var y = message_window_pos.y;
-	var is_reflect = message_window_pos.is_reflect;
-
-	if (!x && !y) return;
-
-	// ウィンドウの大きさ
-	var scale = this._getMessageWindowScale();
-
-	if(is_reflect) {
-		x = -x; // 反転
-		ctx.transform(-1, 0, 0, 1, fukidashi.width, 0); // 左右反転
-	}
-
-	ctx.drawImage(fukidashi,
-		x,
-		y,
-		fukidashi.width * scale.width,
-		fukidashi.height * scale.height
-	);
-	ctx.restore();
-};
-
-// セリフ表示
-SceneSubStageObjectTalk.prototype._showMessage = function() {
-	var ctx = this.core.ctx;
-
-	// セリフ表示
-	var lines = this._serif.lines();
-
-	if (!lines.length) return;
-
-	ctx.save();
-
-	// セリフの色
-	var font_color = Util.hexToRGBString("#d4c9aa");
-
-	ctx.fillStyle = font_color;
-	ctx.font = "18px 'OradanoGSRR'";
-	ctx.textAlign = 'left';
-	ctx.textBaseAlign = 'middle';
-
-	var message_text_pos = this._getMessageTextPos();
-	var x = message_text_pos.x;
-	var y = message_text_pos.y;
-
-	for(var i = 0, len = lines.length; i < len; i++) {
-		y+= 30;
-		ctx.fillText(lines[i], x, y); // 1行表示
-
-	}
+		// TODO:
+	/*
 	// クリック待ちカーソル表示
 	if (this._serif.isWaitingNext()) {
 
@@ -171,119 +108,19 @@ SceneSubStageObjectTalk.prototype._showMessage = function() {
 			cursor.height*2/3
 		);
 	}
+	*/
 
-	ctx.restore();
-};
 
-// セリフ表示を右に表示させるかどうか
-SceneSubStageObjectTalk.prototype._isShowRight = function(){
-	var chara_name = this._serif.getChara();
-	if (chara_name === "koishi") {
-		var x = this.root().koishi.x();
-
-		var scene_center_x = this.root().width / 2;
-
-		if (x > scene_center_x) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	else if (chara_name === "me") {
-		// オブジェクトが会話する場合
-		// TODO:
-		throw new Error("TODO:");
-	}
-	else {
-		return false;
 	}
 };
 
-// メッセージウィンドウの位置を取得
-SceneSubStageObjectTalk.prototype._getMessageWindowPos = function(){
+// セリフ表示
+SceneSubStageObjectTalk.prototype._showMessage = function() {
 	var chara_name = this._serif.getChara();
 
-	var is_reflect = !this._isShowRight();
+	var obj = this.root().getPiece(chara_name);
 
-	var x,y;
-
-	if (chara_name === "koishi") {
-		if(is_reflect) {
-			x = this.root().koishi.x() - 550;
-			y = this.root().koishi.y() - 330;
-		}
-		else {
-			x = this.root().koishi.x() - 300;
-			y = this.root().koishi.y() - 330;
-		}
-	}
-	else if (chara_name === "me") {
-		// オブジェクトが会話する場合
-		// TODO:
-		throw new Error("TODO:");
-	}
-	else {
-		x = 0;
-		y = 0;
-	}
-	return {
-		is_reflect: is_reflect,
-		x: x,
-		y: y,
-	};
+	obj.showMessage(this._serif.lines(), this._serif.isWaitingNext());
 };
-
-// ウィンドウの大きさ
-SceneSubStageObjectTalk.prototype._getMessageWindowScale = function(){
-	var scale_width, scale_height;
-	if (this._serif.getSerifRowsCount() === 2) {
-		scale_width = 0.4;
-		scale_height = 0.5;
-	}
-	else {
-		scale_width = 0.4;
-		scale_height = 0.4;
-	}
-	return {width: scale_width, height: scale_height};
-};
-
-// メッセージテキストの開始位置を取得
-SceneSubStageObjectTalk.prototype._getMessageTextPos = function(){
-	var chara_name = this._serif.getChara();
-
-	var is_reflect = !this._isShowRight();
-
-	var x,y;
-
-	if (chara_name === "koishi") {
-		if(is_reflect) {
-			x = this.root().koishi.x() + 80;
-			y = this.root().koishi.y() - 290;
-		}
-		else {
-			x = this.root().koishi.x() - 220;
-			y = this.root().koishi.y() - 290;
-		}
-
-	}
-	else if (chara_name === "me") {
-		// オブジェクトが会話する場合
-		// TODO:
-		throw new Error("TODO:");
-	}
-	else {
-		throw new Error("Unrecognized chara name: " . chara_name);
-	}
-
-	return {
-		is_reflect: is_reflect,
-		x: x,
-		y: y,
-	};
-};
-
-
-
 
 module.exports = SceneSubStageObjectTalk;
