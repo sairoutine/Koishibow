0fdbeb31cc77c39cd78e88402ed74139be753780
sairoutine <sairoutine@gmail.com>
2018-02-05 04:17:15 +0900

    add: journal config

---
+++ src/js/config/journal.js
@@ -0,0 +1,15 @@
+'use strict';
+//var CONSTANT = require('../constant');
+
+var Journal = {};
+
+/*
+Item[CONSTANT.ITEM.EYEDROPS] = {
+	name: "目薬",
+	image_name: "item_01",
+	selected_image_name: "item_01_selected",
+	description: "使うと目の充血がやわらぐ",
+};
+*/
+
+module.exports = Journal;
