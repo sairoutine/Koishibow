0b260c502d570aa044d27ebc6854de7608bb4478
sairoutine <sairoutine@gmail.com>
2018-02-18 14:46:52 +0900

    update: description of keyboard

--- src/js/game.js
+++ src/js/game.js
@@ -64,7 +64,9 @@ Game.prototype.setupDebug = function (dom) {
 	this.debug_manager.setOn(dom);
 
 	// テキスト追加
-	this.debug_manager.addMenuText("マウスクリックで移動／調べる");
+	this.debug_manager.addMenuText("Zキー：決定、調べる");
+	this.debug_manager.addMenuText("Xキー：キャンセル、サードアイ使用／解除");
+	this.debug_manager.addMenuText("SPACEキー：メニュー開く／閉じる");
 
 	this.debug_manager.addMenuImage("https://api.travis-ci.org/sairoutine/Koishibow.svg?branch=gh-pages");
 
