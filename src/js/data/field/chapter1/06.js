9dab1e350a0e5dddd6973a7abf46841d9fa45e9e
sairoutine <sairoutine@gmail.com>
2018-03-28 03:03:13 +0900

    add: chapter1 field 06

---
+++ src/js/data/field/chapter1/06.js
@@ -0,0 +1,75 @@
+'use strict';
+// 小鈴ショップ
+var CONSTANT = require("../../../constant");
+var I = 0;
+module.exports = {
+	key: "chapter1_06",
+	chapter: 1,
+	bgm: "silent",
+	right_start_position: {x: 690, y: 540},
+	left_start_position:  {x: 180, y: 540},
+	right_field: null,
+	left_field: "chapter1_05",
+	background: "chapter1-06-bg-001",
+	walk_sound: "walking_bare_default",
+	objects: [
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "背景の木々",
+			serif: null,
+			x: 960/2, y: 720/2,
+			scale: 2/3,
+			anime1: "chapter1-06-obj-01-obj01",
+			anime2: null,
+			anime3: null,
+			anime4: "chapter1-06-obj-01-obj02",
+			anime5: "chapter1-06-obj-01-obj03",
+			anime6: "chapter1-06-obj-01-obj04",
+
+			action_name: null,
+			sound_name: null,
+			position_type: "lying",
+		},
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "街灯",
+			serif: [
+				{"chara": "koishi","serif":"test"},
+			],
+			x:1296.50*2/3, y:344*2/3,
+			scale: 2/3,
+			anime1: "chapter1-06-obj-04-obj01",
+			action_name: null,
+			sound_name: null,
+		},
+		{
+			no: ++I,
+			image: "chapter1-06-obj-02",
+			type: CONSTANT.STATIC_IMAGE_TYPE,
+			name: "石垣(右)",
+			serif: [
+				{"chara": "koishi","serif":"test"},
+			],
+			x:1356.50*2/3, y:603*2/3,
+			scale: 2/3,
+			action_name: null,
+			sound_name: null
+		},
+		{
+			no: ++I,
+			image: "chapter1-06-obj-03",
+			type: CONSTANT.STATIC_IMAGE_TYPE,
+			name: "石垣(左)",
+			serif: [
+				{"chara": "koishi","serif":"test"},
+			],
+			x:434.50*2/3, y:600*2/3,
+			scale: 2/3,
+			action_name: null,
+			sound_name: null
+		},
+	],
+};
+
