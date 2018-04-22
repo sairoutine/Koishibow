0c39214cf09304bfba1dc60054c9bb19c1a44d29
sairoutine <sairoutine@gmail.com>
2018-04-23 00:37:17 +0900

    add: chapter1 field 09 data

---
+++ src/js/data/field/chapter1/09.js
@@ -0,0 +1,76 @@
+'use strict';
+var CONSTANT = require("../../../constant");
+var I = 0;
+module.exports = {
+	key: "chapter1_09",
+	chapter: 1,
+	bgm: "field4",
+	right_start_position: {x: 690, y: 380},
+	left_start_position:  {x: 180, y: 380},
+	right_field: "chapter1_10",
+	left_field: "chapter1_08",
+	background: "chapter1-09-bg-001",
+	walk_sound: "walking_bare_outside",
+	objects: [
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "生垣(秘密基地)",
+			serif: null,
+			x: 720*2/3, y: 428*2/3,
+			scale: 2/3,
+			anime1: "chapter1-09-obj-02-obj01",
+			anime2: "chapter1-09-obj-02-obj02",
+			anime3: "chapter1-09-obj-02-obj01",
+
+			action_name: null,
+			sound_name: null,
+		},
+
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "村紗",
+			serif: [
+				// 1-16
+				{"chara": I, "exp": "murasa-reaction_01", "serif":"何"},
+				{"chara": "koishi",        "exp": "back", "serif":"えっ？"},
+				{"chara": I, "exp": "murasa-reaction_01", "serif":"なんだよ"},
+				{"chara": I, "exp": "murasa-reaction_02", "serif":"用がないならあっちいけよ"},
+				{"chara": "koishi",        "exp": "back", "serif":"あそばないの？"},
+				{"chara": I, "exp": "murasa-reaction_01", "serif":"は？"},
+				{"chara": I, "exp": "murasa-reaction_01", "serif":"もう遊んでるだろ"},
+				{"chara": "koishi",        "exp": "back", "serif":"............"},
+				{"chara": I, "exp": "murasa-reaction_02", "serif":"なんだよ！あっちいけよ！"},
+			],
+			x: 1198*2/3, y: 588*2/3,
+			scale: 2/3,
+			anime1: "murasa-wait",
+			anime2: null,
+			anime3: null,
+			anime4: "murasa-reverse_ura",
+			anime5: "murasa-ura_wait",
+			anime6: "murasa-reverse_omote",
+
+			action_name: null,
+			sound_name: null,
+		},
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "蛇口",
+			serif: null,
+			x:384*2/3, y:572*2/3,
+			scale: 2/3,
+			anime1: "chapter1-09-obj-01-obj01",
+			anime2: null,
+			anime3: null,
+			anime4: "chapter1-09-obj-01-obj02",
+			anime5: "chapter1-09-obj-01-obj03",
+			anime6: "chapter1-09-obj-01-obj04",
+
+			action_name: null,
+			sound_name: null,
+		},
+	]
+};
