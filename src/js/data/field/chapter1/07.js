bb5a7cdcbbc91ca2c2c700a6fa72881b7e2eaf4d
sairoutine <sairoutine@gmail.com>
2018-04-22 23:14:31 +0900

    update: add chapter1 field 07

---
+++ src/js/data/field/chapter1/07.js
@@ -0,0 +1,28 @@
+'use strict';
+var CONSTANT = require("../../../constant");
+var I = 0;
+module.exports = {
+	key: "chapter1_07",
+	chapter: 1,
+	bgm: "field4",
+	right_start_position: {x: 690, y: 380},
+	left_start_position:  {x: 180, y: 380},
+	right_field: "chapter1_08",
+	left_field: "chapter1_06",
+	background: "chapter1-07-bg-001",
+	walk_sound: "walking_bare_outside",
+	objects: [
+		{
+			no: ++I,
+			image: "chapter1-07-obj-01",
+			type: CONSTANT.STATIC_IMAGE_TYPE,
+			name: "ボール",
+			serif: null,
+			x: 625*2/3, y: 768.50*2/3,
+			scale: 2/3,
+			action_name: "touch",
+			sound_name: "touch"
+		},
+	]
+};
+
