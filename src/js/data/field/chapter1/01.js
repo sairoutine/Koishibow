708e76ce72a5f78eadcf6d6129a974af3f32917d
sairoutine <sairoutine@gmail.com>
2018-04-28 01:57:08 +0900

    update: delete sub event of opening movie

--- src/js/data/field/chapter1/01.js
+++ src/js/data/field/chapter1/01.js
@@ -12,7 +12,6 @@ module.exports = {
 	left_field: null,
 	background: "chapter1-01-bg-001",
 	walk_sound: "walking_bare_outside",
-	event: "event_opening_movie_chapter1",
 	objects: [
 		{
 			no: ++I,
