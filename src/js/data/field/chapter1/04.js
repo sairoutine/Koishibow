d1754645477e5c9d863d17cb9224bec9b7b43eb5
sairoutine <sairoutine@gmail.com>
2018-05-03 02:30:14 +0900

    update: ring phone

--- src/js/data/field/chapter1/04.js
+++ src/js/data/field/chapter1/04.js
@@ -35,7 +35,95 @@ module.exports = {
 			no: ++I,
 			type: CONSTANT.ANIME_IMAGE_TYPE,
 			name: "電話ボックス赤",
-			serif: null,
+			loop: true, // 何回でもタッチできる
+			serif: [
+				{"type": "criteria_serif", "criteria": "circulate", "arguments": ["chapter1_04-" + I + "circulate", 19], "serifs": [
+					[
+						{"chara": I,"serif":"ぷーーーー"},
+					],
+					[
+						{"chara": I,"serif":"ぷーっぷーっぷーっ"},
+					],
+					[
+						{"chara": I,"serif":"泣いてる子だあれ？泣いてる子は迎えに行くよ"},
+					],
+					[
+						{"chara": I,"serif":"らりるれろ！らりるれろ！らりるれろ！"},
+					],
+					[
+						{"chara": I,"serif":"そこで見てるのだれ？"},
+					],
+					[
+						{"chara": I,"serif":"もしもし、どなた？"},
+					],
+					[
+						{"chara": I,"serif":"もしもし？"},
+					],
+					[
+						{"chara": I,"serif":"あなたなの？"},
+					],
+					[
+						{"chara": I,"serif":"はい わたしです"},
+						{"chara": I,"serif":"はい"},
+						{"chara": I,"serif":"問題なく"},
+						{"chara": I,"serif":"はい"},
+						{"chara": I,"serif":"すべてをやり直せます"},
+						{"chara": I,"serif":"彼女はいまも眠っています"},
+						{"chara": I,"serif":"心的外傷を癒す時が来たのです"},
+						{"chara": I,"serif":"彼女こそあなたの”ソフィー”です"},
+					],
+					[
+						{"chara": I,"serif":"もしもし？"},
+					],
+					[
+						{"chara": I,"serif":"迷惑なんですけど！"},
+					],
+					[
+						{"chara": I,"serif":"ひょっとしてメリーさん？"},
+					],
+					[
+						{"chara": I,"serif":"やめなさいよ！"},
+					],
+					[
+						{"chara": I,"serif":"もうかけてこないで！"},
+					],
+					[
+						{"chara": I,"serif":"ごめんなさい！"},
+					],
+					[
+						{"chara": I,"serif":"おねがいします！"},
+					],
+					[
+						{"chara": I,"serif":"おかけになった電話番号は\n現在使われておりません"},
+					],
+					[
+						{"chara": I,"serif":"天気予告をお知らせいたします。"},
+						{"chara": I,"serif":"明日の天気はハレ、明日の天気はハレ"},
+					],
+					[
+						{"chara": I,"serif":"こんにちは"},
+						{"chara": I,"serif":"おでんわありがとう！"},
+						{"chara": I,"serif":"おはなししましょ！"},
+						{"chara": I,"serif":"ばいばーい！"},
+					],
+					[
+						{"chara": I,"serif":"火事ですか救急ですか？"},
+						{"chara": "koishi","serif":"あのね、何でもないです"},
+					],
+					[
+						{"chara": I,"serif":"火事ですか救急ですか？"},
+						{"chara": I,"serif":"あのね、何でもないです"},
+					],
+					[
+						{"chara": I,"serif":"申します申します、どちらへお繋しますか？"},
+					],
+					[
+						{"chara": I,"serif":"おかあさん"},
+						{"chara": I,"serif":"おかあさん"},
+						{"chara": I,"serif":"どうして？"},
+					],
+				]}
+			],
 			x:805, y:308,
 			scale: 2/3,
 			anime1: "chapter1-04-obj-02-obj01",
