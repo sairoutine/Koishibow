d9b41e5f906c5fdfe3e45f354249230315b62e14
sairoutine <sairoutine@gmail.com>
2018-05-04 03:01:45 +0900

    update: enable to get item twice

--- src/js/storage/item.js
+++ src/js/storage/item.js
@@ -23,41 +23,66 @@ StorageItem.prototype.getItemList = function() {
 	return list;
 };
 
+/*
 // 取得しているアイテム一覧を取得
 StorageItem.prototype.getItem = function(index){
 	var list = this.getItemList();
 
 	return list[index];
 };
+*/
 
 // アイテムを追加(追加したアイテムのindexを返す)
 StorageItem.prototype.addItem = function(item_id){
 	var list = this.getItemList();
 
-	list.push(item_id);
+	for (var i = 0, len = list.length; i < len; i++) {
+		var item_data = list[i];
+
+		// 既存を追加
+		if(item_data.item_id === item_id) {
+			item_data.num++;
+			this.set("item_list", list);
+			return i;
+		}
+	}
+
+	// 新規追加
+	list.push({
+		item_id: item_id,
+		num: 1,
+	});
 	this.set("item_list", list);
 	return list.length - 1;
 };
-// アイテムを削除
-StorageItem.prototype.deleteItem = function(target_item_id) {
+// アイテムを消費
+StorageItem.prototype.reduceItem = function(target_item_id) {
 	var list = this.getItemList();
 
-	var is_delete = false;
+	var is_reduce = false;
 	for (var i = 0, len = list.length; i < len; i++) {
-		var item_id = list[i];
+		var item_data = list[i];
 
-		if (item_id === target_item_id) {
-			list.splice(i, 1);
-			is_delete = true;
+		if (item_data.item_id === target_item_id) {
+			// 最後の一つを削除
+			if (item_data.num === 1) {
+				list.splice(i, 1);
+			}
+			// 一つだけ消費
+			else {
+				item_data.num--;
+			}
+
+			is_reduce = true;
 			break;
 		}
 	}
 
-	if (is_delete) {
+	if (is_reduce) {
 		this.set("item_list", list);
 	}
 
-	return is_delete;
+	return is_reduce;
 };
 
 // アイテムの存在チェック
@@ -65,9 +90,9 @@ StorageItem.prototype.existsItem = function(target_item_id) {
 	var list = this.getItemList();
 
 	for (var i = 0, len = list.length; i < len; i++) {
-		var item_id = list[i];
+		var item_data = list[i];
 
-		if (item_id === target_item_id) {
+		if (item_data.item_id === target_item_id) {
 			return true;
 		}
 	}
