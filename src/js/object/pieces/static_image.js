ca8155fae95f95fae642248984236d42bd2f3b22
sairoutine <sairoutine@gmail.com>
2018-05-05 03:41:01 +0900

    update: static image

--- src/js/object/pieces/static_image.js
+++ src/js/object/pieces/static_image.js
@@ -10,6 +10,8 @@ var ObjectStaticImage = function(core) {
 	this._sound_name  = null;
 	this._action_name = null;
 
+	this._not_show_if_event_true = null;
+
 	this._scale = 1;
 	this._width  = null;
 	this._height = null;
@@ -60,6 +62,11 @@ ObjectStaticImage.prototype.setData = function(data) {
 		this._action_name  = data.action_name;
 	}
 
+	// 表示条件イベント(ここで指定されたイベントを見ていれば表示されない)
+	if (data.not_show_if_event_true) {
+		this._not_show_if_event_true = data.not_show_if_event_true;
+	}
+
 	if (data.width) {
 		this._width  = data.width;
 	}
@@ -71,8 +78,12 @@ ObjectStaticImage.prototype.setData = function(data) {
 	}
 };
 ObjectStaticImage.prototype.isShow = function(){
-	return true;
+	return this._not_show_if_event_true && this.core.save_manager.scenario.getPlayedCount(this._not_show_if_event_true) ? false : true;
 };
+ObjectStaticImage.prototype.isCollision = function(){
+	return this._not_show_if_event_true && this.core.save_manager.scenario.getPlayedCount(this._not_show_if_event_true) ? false : true;
+};
+
 ObjectStaticImage.prototype.draw = function(){
 	base_object.prototype.draw.apply(this, arguments);
 	if(!this.isShow()) return;
