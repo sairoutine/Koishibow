b39a558c569e0ca12090fd1e4c777f4c3a8ca66a
sairoutine <sairoutine@gmail.com>
2018-05-08 03:32:32 +0900

    update: use headless chrome instead of phantomjs

--- test/headless.js
+++ test/headless.js
@@ -1,69 +1,25 @@
-/* global phantom */
 'use strict';
 
-var url = "https://sairoutine.github.io/Koishibow/public/";
-//var url = "http://localhost:3000/";
+const puppeteer = require('puppeteer');
 
-// from https://github.com/ariya/phantomjs/blob/master/examples/waitfor.js
-
-/**
- * Wait until the test condition is true or a timeout occurs. Useful for waiting
- * on a server response or for a ui change (fadeIn, etc.) to occur.
- *
- * @param testFx javascript condition that evaluates to a boolean,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param onReady what to do when testFx condition is fulfilled,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
- */
-
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function() {
-            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-            } else {
-                if(!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
-                }
-            }
-        }, 250); //< repeat check every 250ms
-}
-
-
-
-var page = require('webpage').create();
-page.onConsoleMessage = function(msg) {
-	console.log(msg);
-};
-page.open(url, function(status) {
-	console.log("Status: " + status);
-	if(status === "success") {
-		waitFor(function() {
-			return page.evaluate(function() {
-				return window.game && window.game.isAllLoaded();
-			});
-			}, function() {
-				// success
-				phantom.exit(0);
-			},
-			10000
-		);
-	}
+process.on('unhandledRejection', () => {
+	console.error("timeout");
+	process.exit(1);
 });
 
 
+const url = "https://sairoutine.github.io/Koishibow/public/";
+//const url = "http://localhost:3000/";
+(async() => {
+	const browser = await puppeteer.launch({args: ['--no-sandbox']});
+	const page = await browser.newPage();
+	page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
+	await page.goto(url);
+	const watchDog = page.waitForFunction( () => {
+		return window.game && window.game.isAllLoaded();
+	}, {timeout: 100000});
 
+	await watchDog;
+	await browser.close();
+})();
