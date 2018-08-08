bb8293e1613a9564385d0787c5bc0833802e3349
sairoutine <sairoutine@gmail.com>
2018-08-09 05:46:54 +0900

    update: delete comment

--- src/js/data/field/chapter1/05.js
+++ src/js/data/field/chapter1/05.js
@@ -79,7 +79,6 @@ module.exports = {
 						{"chara": "koishi", "exp": "back", "serif":"えっ？"},
 						{"chara": "koishi", "exp": "back", "serif":"わかんない......"},
 						// 1-14
-						// TODO: 別の会話に切り出す
 						{"chara": I,        "exp": "nitori-reaction_04", "serif":"お面がほしいの？"},
 						{"chara": I,        "exp": "nitori-reaction_04", "serif":"売れ残りだけど、あげちゃう！", "option": {
 							"getItem": "10",
