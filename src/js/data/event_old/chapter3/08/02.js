cbf3971013b34101331933a5eed8d44fee5c0391
sairoutine <sairoutine@gmail.com>
2018-08-09 18:29:14 +0900

    update se

--- src/js/data/event_old/chapter3/08/02.js
+++ src/js/data/event_old/chapter3/08/02.js
@@ -30,14 +30,23 @@ module.exports = {
 		],
 	},
 	serifs: [
-		{"exp": "chapter3-08-event-02-obj01", "serif":"よいしょ", "option": {"x": 700, "y": 200, "loop": false}},
-		{"exp": "chapter3-08-event-02-obj02", "serif":"うん", "option": {"x": 700, "y": 200, "loop": false}},
+		{"exp": "chapter3-08-event-02-obj01", "serif":"よいしょ", "option": {"x": 700, "y": 200, "loop": false, "sounds": [
+				{"name": "chapter3-08-event-02-01", "frame": 0},
+		]}},
+
+		{"exp": "chapter3-08-event-02-obj02", "serif":"うん", "option": {"x": 700, "y": 200, "loop": false, "sounds": [
+				{"name": "chapter3-08-event-02-02", "frame": 30},
+		]}},
 		{"exp": null, "serif":"ぐえ", "option": {"x": 470, "y": 180, "loop": false}},
 		{"exp": null, "serif":"えい", "option": {"x": 700, "y": 200, "loop": false}},
 		{"exp": null, "serif":"ぐえ", "option": {"x": 470, "y": 180, "loop": false}},
-		{"exp": "chapter3-08-event-02-obj03", "serif":"えい", "option": {"x": 700, "y": 200, "loop": false}},
+		{"exp": "chapter3-08-event-02-obj03", "serif":"えい", "option": {"x": 700, "y": 200, "loop": false, "sounds": [
+				{"name": "chapter3-08-event-02-03", "frame": 0},
+		]}},
 		{"exp": null, "serif":"ぐええっ", "option": {"x": 470, "y": 0, "loop": false}},
-		{"exp": "chapter3-08-event-02-obj04", "serif":"やった！", "option": {"x": 700, "y": 200, "loop": false}},
+		{"exp": "chapter3-08-event-02-obj04", "serif":"やった！", "option": {"x": 700, "y": 200, "loop": false, "sounds": [
+				{"name": "chapter3-08-event-02-04", "frame": 45},
+		]}},
 		{"exp": null, "serif":"うげっ", "option": {"x": 470, "y": 350, "loop": false}},
 	],
 	endProcess: [
