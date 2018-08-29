e35ff427dae381389a764ebdacf6c0a1c8df9db9
sairoutine <sairoutine@gmail.com>
2018-08-30 00:07:29 +0900

    update: show cursor

--- src/js/scene/substage/event/chapter0/get_hat.js
+++ src/js/scene/substage/event/chapter0/get_hat.js
@@ -3,15 +3,14 @@
 var base_scene = require('../../base');
 var Util = require('../../../../hakurei').util;
 
+var SelectedCursor = require('../../../../object/ui/selected_cursor');
 var CONSTANT_BUTTON = require('../../../../hakurei').constant.button;
 var ObjectChapter0Hat = require('../../../../object/pieces/chapter0_hat');
 
 var SceneEventChapter0GetHat = function(core) {
 	base_scene.apply(this, arguments);
 
-	// カーソルの状態
-	this._cursor_y = 0;
-	this._cursor_reverse = false;
+	this._cursor_ui = new SelectedCursor(this);
 
 	// 帽子オブジェクト
 	this.hat = new ObjectChapter0Hat(this);
@@ -22,9 +21,7 @@ Util.inherit(SceneEventChapter0GetHat, base_scene);
 SceneEventChapter0GetHat.prototype.init = function(){
 	base_scene.prototype.init.apply(this, ["chapter0_myroom"]);
 
-	// カーソルの状態
-	this._cursor_y = 0;
-	this._cursor_reverse = false;
+	this._cursor_ui.init();
 
 	this.hat.setData({
 		no: 1,
@@ -41,15 +38,16 @@ SceneEventChapter0GetHat.prototype.init = function(){
 SceneEventChapter0GetHat.prototype.beforeDraw = function() {
 	base_scene.prototype.beforeDraw.apply(this, arguments);
 
+	this._cursor_ui.beforeDraw();
 	// こいしの移動
 	this.root().koishi.moveByInput();
 
-	if (this.hat.checkIsInTouchArea(this.root().koishi)) {
-		if (this.core.input_manager.isKeyPush(CONSTANT_BUTTON.BUTTON_Z)) {
+	// タッチできるオブジェクトであることを示すカーソル表示
+	this._showSelectedCursor(this.hat);
+
+	if (this.core.input_manager.isKeyPush(CONSTANT_BUTTON.BUTTON_Z)) {
+		if (this.hat.checkIsInTouchArea(this.root().koishi)) {
 			this.hat.onTouchByKoishi();
-		}
-		else {
-			// TODO: 調べられるよカーソルを表示
 		}
 	}
 };
@@ -58,34 +56,13 @@ SceneEventChapter0GetHat.prototype.beforeDraw = function() {
 SceneEventChapter0GetHat.prototype.draw = function(){
 	base_scene.prototype.draw.apply(this, arguments);
 
-	// 帽子オブジェクトの描画
-
-	// カーソルを上下に移動させる
-	if (this._cursor_reverse) {
-		this._cursor_y-=0.25;
-	}
-	else {
-		this._cursor_y+=0.25;
-	}
-
-	if (this._cursor_y > 6) {
-		this._cursor_reverse = true;
-	}
-	else if (this._cursor_y === 0) {
-		this._cursor_reverse = false;
-	}
-
-	// 黒い矢印
-	var cursor = this.core.image_loader.getImage("target_arrow");
-	var ctx = this.core.ctx;
-	ctx.save();
-	ctx.drawImage(cursor,
-		747, // 18 -> font size
-		479 + this._cursor_y, // TODO: 要調整
-		cursor.width*2/3,
-		cursor.height*2/3
-	);
-	ctx.restore();
+	this._cursor_ui.draw();
 };
+// タッチできるオブジェクトであることを示すカーソル表示
+SceneEventChapter0GetHat.prototype._showSelectedCursor = function(piece) {
+	this._cursor_ui.setPosition(piece.x(), piece.y() - 50);
+	this._cursor_ui.show();
+};
+
 
 module.exports = SceneEventChapter0GetHat;
