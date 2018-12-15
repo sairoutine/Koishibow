23664dc8ddd0f0ced20e496dcafff0081ae73fe3
sairoutine <sairoutine@gmail.com>
2018-12-16 04:22:12 +0900

    fix: image name

--- src/js/config/assets/production.js
+++ src/js/config/assets/production.js
@@ -15,7 +15,7 @@ AssetsConfig.images = {
     // 優曇華院
 	"reisen":   "./image/production/reisen.png",
     // てゐリアル
-	"reisen":   "./image/production/tewireal.png",
+	"tewireal":   "./image/production/tewireal.png",
 
 	// 3-1 こいしが起きる
 	"chapter3-01-event-01-koishi":  "./image/production/chapter3/event/chapter3-01-event-01/chapter3-01-event-01-koishi.png",
