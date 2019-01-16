cad506cfc02f43c64be280063ea2a1d8d5742d67
sairoutine <sairoutine@gmail.com>
2019-01-17 08:24:59 +0900

    implement: chapter5 03

--- src/js/data/field/chapter5/03.js
+++ src/js/data/field/chapter5/03.js
@@ -1,9 +1,8 @@
 'use strict';
+var _ = require('i18n4v')
 // 地下1階
-/*
-斧
-下り階段
-*/
+var CONSTANT = require("../../../constant");
+var I = 0;
 module.exports = {
 	key: "chapter5_03",
 	chapter: 5,
@@ -12,13 +11,61 @@ module.exports = {
 	leftStartPosition:  {x: 140, y: 480},
 	rightField: null,
 	leftField: "chapter5_02",
-	background: "chapter5-03-bg-001",
+	background: null,
 	walkSound: "walking_bare_default",
 	initialProcess: [
 	],
 	objects: [
-		// 上り階段で5-7へ
-		// 下り階段で5-4へ
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "背景",
+			serif: null,
+			x: 960/2, y: 720/2,
+			scale: 2/3,
+			anime1: "chapter5-03-bg-01_anime_1",
+			anime2: null,
+			anime3: null,
+			anime4: null,
+			anime5: null,
+			anime6: null,
+			action_name: null,
+			sound_name: null,
+			position_type: "lying",
+		},
+		{
+			no: ++I,
+			image: "chapter5-03-obj-01",
+			type: CONSTANT.STATIC_IMAGE_TYPE,
+			name: "斧",
+			serif: [
+				{"chara": "koishi","serif":_("test")},
+			],
+			x: 282, y: 447,
+			scale: 2/3,
+			action_name: "look_bottom",
+			sound_name: null
+		},
+		{
+			no: ++I,
+			type: CONSTANT.FIELD_CHANGE_TYPE,
+			name: "上り階段",
+			x: 800,
+			y: 260,
+			width: 300,
+			height: 400,
+			next_field_name: "chapter5_07",
+		},
+		{
+			no: ++I,
+			type: CONSTANT.FIELD_CHANGE_TYPE,
+			name: "下り階段",
+			x: 910,
+			y: 730,
+			width: 250,
+			height: 550,
+			next_field_name: "chapter5_04",
+		},
 	],
 };
 
