cc501ff695d2f9d1ee57c554efeae4125439c064
sairoutine <sairoutine@gmail.com>
2019-01-18 07:09:12 +0900

    fix: right position

--- src/js/data/field/chapter5/03.js
+++ src/js/data/field/chapter5/03.js
@@ -7,7 +7,7 @@ module.exports = {
 	key: "chapter5_03",
 	chapter: 5,
 	bgm: "silent",
-	rightStartPosition: {x: 790, y: 480},
+	rightStartPosition: {x: 700, y: 480},
 	leftStartPosition:  {x: 140, y: 480},
 	rightField: null,
 	leftField: "chapter5_02",
