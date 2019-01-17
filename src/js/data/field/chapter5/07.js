2c2c8fedd946416cdcfcd5815f666c315f86e86a
sairoutine <sairoutine@gmail.com>
2019-01-18 07:09:37 +0900

    implement: chapter5 field 08

--- src/js/data/field/chapter5/07.js
+++ src/js/data/field/chapter5/07.js
@@ -1,10 +1,8 @@
 'use strict';
+var _ = require('i18n4v')
 // 地上1階
-/*
-いぬ
-はしご（設置後）
-コモドオオトカゲ
-*/
+var CONSTANT = require("../../../constant");
+var I = 0;
 module.exports = {
 	key: "chapter5_07",
 	chapter: 5,
@@ -13,13 +11,113 @@ module.exports = {
 	leftStartPosition:  {x: 140, y: 480},
 	rightField: null,
 	leftField: "chapter5_08",
-	background: "chapter5-07-bg-001",
+	background: null,
 	walkSound: "walking_bare_default",
 	initialProcess: [
 	],
 	objects: [
-		// 上り階段で5-11へ
-		// 下り階段で5-3へ
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "背景",
+			serif: null,
+			x: 960/2, y: 720/2,
+			scale: 2/3,
+			anime1: "chapter5-07-bg-01_anime_1",
+			anime2: null,
+			anime3: null,
+			anime4: null,
+			anime5: null,
+			anime6: null,
+			action_name: null,
+			sound_name: null,
+			position_type: "lying",
+		},
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "イヌ",
+			loop: true,
+			serif_back: [
+				{"chara": I, "exp": "animal_shiba-ura_reaction_01", "serif":_("test")},
+			],
+			serif: [
+				{"chara": I, "exp": "animal_shiba-wait", "serif":_("test")},
+			],
+			x: 600, y: 300,
+			scale: 2/3,
+			anime1: "animal_shiba-wait",
+			anime2: null,
+			anime3: null,
+			anime4: "animal_shiba-reverse_ura",
+			anime5: "animal_shiba-ura_wait",
+			anime6: "animal_shiba-reverse_omote",
+			width:  150,
+			height: 350,
+			action_name: null,
+			sound_name: null,
+		},
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "コモドオオトカゲ",
+			loop: true,
+			serif_back: [
+				{"chara": I, "exp": "animal_komodo-ura_reaction_01", "serif":_("test")},
+			],
+			serif: [
+				{"chara": I, "exp": "animal_komodo-wait", "serif":_("test")},
+			],
+			x: 430, y: 500,
+			scale: 2/3,
+			anime1: "animal_komodo-wait",
+			anime2: null,
+			anime3: null,
+			anime4: "animal_komodo-reverse_ura",
+			anime5: "animal_komodo-ura_wait",
+			anime6: "animal_komodo-reverse_omote",
+			width:  150,
+			height: 200,
+			action_name: null,
+			sound_name: null,
+		},
+		{
+			no: ++I,
+			type: CONSTANT.FIELD_CHANGE_TYPE,
+			name: "下り階段",
+			x: 910,
+			y: 530,
+			width: 250,
+			height: 550,
+			next_field_name: "chapter5_03",
+		},
+		// はしごを使って 5-11へ
+		{
+			no: ++I,
+			image: "chapter1-03-obj-03",
+			type: CONSTANT.STATIC_IMAGE_TYPE,
+			name: "はしご",
+			serif: null,
+			x:40, y:240,
+			//width: 200,
+			//height: 200,
+			scale: 2/3,
+			action_name: null,
+			sound_name: null
+		},
+		{
+			no: ++I,
+			type: CONSTANT.FIELD_CHANGE_TYPE,
+			name: "はしご",
+			x:40, y:240,
+			width: 130,
+			height: 361,
+			next_field_name: "chapter5_11",
+		},
+
+
+
+
 	],
 };
 
