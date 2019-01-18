08f4c8db80b1a11528a135315162dec42cce1a64
sairoutine <sairoutine@gmail.com>
2019-01-19 05:36:10 +0900

    implement: chapter5 field 09

--- src/js/data/field/chapter5/09.js
+++ src/js/data/field/chapter5/09.js
@@ -1,6 +1,9 @@
 'use strict';
+//var _ = require('i18n4v')
 // 地上2階
 // お空が閉じ込められている部屋
+//var CONSTANT = require("../../../constant");
+//var I = 0;
 module.exports = {
 	key: "chapter5_09",
 	chapter: 5,
@@ -14,6 +17,32 @@ module.exports = {
 	initialProcess: [
 	],
 	objects: [
+		/*
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "おくう",
+			loop: true,
+			serif_back: [
+				{"chara": I, "exp": "oku-ura_reaction_01", "serif":_("test")},
+			],
+			serif: [
+				{"chara": I, "exp": "oku-wait", "serif":_("test")},
+			],
+			x: 600, y: 300,
+			scale: 2/3,
+			anime1: "oku-wait",
+			anime2: null,
+			anime3: null,
+			anime4: "oku-reverse_ura",
+			anime5: "oku-ura_wait",
+			anime6: "oku-reverse_omote",
+			width:  150,
+			height: 350,
+			action_name: null,
+			sound_name: null,
+		},
+		*/
 	],
 };
 
