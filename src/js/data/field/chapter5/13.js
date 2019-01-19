0a5984ecdbdc33cab338b4e4d6ebede265bf9410
sairoutine <sairoutine@gmail.com>
2019-01-19 23:21:43 +0900

    implement: chapter5 field 13

--- src/js/data/field/chapter5/13.js
+++ src/js/data/field/chapter5/13.js
@@ -1,9 +1,11 @@
 'use strict';
+var _ = require('i18n4v')
 // 地上2階
 /*
-くろひょう
 茨右
 */
+var CONSTANT = require("../../../constant");
+var I = 0;
 module.exports = {
 	key: "chapter5_13",
 	chapter: 5,
@@ -12,11 +14,61 @@ module.exports = {
 	leftStartPosition:  {x: 140, y: 480},
 	rightField: null,
 	leftField: "chapter5_11",
-	background: "chapter5-13-bg-001",
+	background: null,
 	walkSound: "walking_bare_default",
 	initialProcess: [
 	],
 	objects: [
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "背景",
+			serif: null,
+			x: 960/2, y: 720/2,
+			scale: 2/3,
+			anime1: "chapter5-13-bg-01_anime_1",
+			anime2: null,
+			anime3: null,
+			anime4: null,
+			anime5: null,
+			anime6: null,
+			action_name: null,
+			sound_name: null,
+			position_type: "lying",
+		},
+		{
+			no: ++I,
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "ヒョウ",
+			loop: true,
+			serif_back: [
+				{"chara": I, "exp": "animal_kuro-ura_reaction_01", "serif":_("test")},
+			],
+			serif: [
+				{"chara": I, "exp": "animal_kuro-wait", "serif":_("test")},
+			],
+			x: 700, y: 500,
+			scale: 2/3,
+			anime1: "animal_kuro-wait",
+			anime2: null,
+			anime3: null,
+			anime4: "animal_kuro-reverse_ura",
+			anime5: "animal_kuro-ura_wait",
+			anime6: "animal_kuro-reverse_omote",
+			width:  150,
+			height: 350,
+			action_name: null,
+			sound_name: null,
+		},
+		{
+			no: ++I,
+			image: null,
+			type: CONSTANT.STATIC_IMAGE_TYPE,
+			name: "茨",
+			serif: null,
+			width: 330, height: 665,
+			x: 800, y: 275,
+		},
 	],
 };
 
