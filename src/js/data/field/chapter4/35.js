bb9ceacfdc5f2739176d86caa080f5f596e42fb2
sairoutine <sairoutine@gmail.com>
2019-02-25 00:23:15 +0900

    add: chapter4 serifs

--- src/js/data/field/chapter4/35.js
+++ src/js/data/field/chapter4/35.js
@@ -1,5 +1,6 @@
 'use strict';
 var CONSTANT = require("../../../constant");
+var _ = require('i18n4v')
 var I = 0;
 module.exports = {
 	key: "chapter4_35",
@@ -33,6 +34,39 @@ module.exports = {
 			width: 960,
 			height: 10,
 			next_field_name: "chapter4_39",
+		},
+		{
+			no: "merry",
+			type: CONSTANT.ANIME_IMAGE_TYPE,
+			name: "メリー",
+			loop: true,
+			serif_back: [],
+			serif: [
+				{"type": "criteria_serif", "criteria": "circulate", "arguments": ["chapter4_35-merry-circulate", 2], "serifs": [
+					[
+						{"chara": "koishi", "exp": "look_front", "serif":_("ねえ、お友達いっぱいいるの")},
+						{"chara": "merry", "exp": "merry-reaction_09", "serif":_("あんまりいないわね")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("私はたくさんいるよ")},
+						{"chara": "merry", "exp": "merry-reaction_06", "serif":_("素敵ね")},
+						{"chara": "merry", "exp": null, "serif":_("なんて子？")},
+						{"chara": "koishi", "exp": "look_top", "serif":_("なにが？")},
+						{"chara": "merry", "exp": "merry-reaction_09", "serif":_("お友達の名前は？")},
+					],
+					[
+						{"chara": "merry", "exp": "merry-reaction_10", "serif":_("どうしたの")},
+						{"chara": "koishi", "exp": "look_bottom", "serif":_("なんでもない")},
+					],
+				]},
+			],
+			x: 1060*2/3, y: 505*2/3,
+			anime1: "merry-wait",
+			anime2: null,
+			anime3: null,
+			anime4: null,
+			anime5: null,
+			anime6: null,
+			action_name: null,
+			sound_name: null,
 		},
 
 
