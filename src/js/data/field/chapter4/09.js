41e9f9609be9392907c5f337d31c48477f2170f8
sairoutine <sairoutine@gmail.com>
2019-02-25 00:10:19 +0900

    add: chapter4 serifs

--- src/js/data/field/chapter4/09.js
+++ src/js/data/field/chapter4/09.js
@@ -14,6 +14,39 @@ module.exports = {
 	background: "chapter4-09-bg-001",
 	walkSound: "walking_bare_default",
 	initialProcess: [
+		{"type": "criteria", "value": "isPlayed", "arguments": ["chapter4_09_40130"], "process": [
+			// すでにアクセス済み
+			[
+				// event 40140 を再生済かどうか
+				{"type": "criteria", "value": "isPlayed", "arguments": ["chapter4-07-event-40140"], "process": [
+					// すでにアクセス済み
+					[
+						{"chara": "renko", "exp": "renko-reaction_01", "serif":_("ねえ大丈夫なの？")},
+						{"chara": "merry", "exp": "merry-reaction_09", "serif":_("なにが？")},
+						{"chara": "renko", "exp": "renko-reaction_01", "serif":_("なんだか変よ")},
+						{"chara": "merry", "exp": "merry-reaction_08", "serif":_("そうかしら")},
+						{"chara": "renko", "exp": "renko-wait", "serif":_("まあいいわ、先に進みましょ")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("わたしもいい？")},
+						{"chara": "merry", "exp": "merry-reaction_06", "serif":_("いいわよ")},
+						{"chara": "renko", "exp": "renko-wait", "serif":_("......")},
+						{"chara": "renko", "exp": "renko-wait", "serif":_("私先に行くわね"), "option": {"changeField": "chapter4_24"}},
+					],
+					// まだ未アクセス
+					[
+					],
+				]},
+			],
+			// まだ未アクセス
+			[
+				{"chara": "merry", "exp": "merry-reaction_05", "serif":_("ちょっと待って")},
+				{"chara": "koishi", "exp": "look_front", "serif":_("なに？")},
+				{"chara": "merry", "exp": "merry-reaction_09", "serif":_("蓮子を見つけないと")},
+				{"chara": "merry", "exp": "merry-reaction_03", "serif":_("これ以上進むとはぐれたところからどんどん離れちゃう")},
+				{"chara": "koishi", "exp": "yes", "serif":_("わかった")},
+				{"type": "process", "value": "incrementPlayedFlag", "arguments": ["chapter4_09_40130"]}
+			],
+		]},
+
 	],
 	objects: [
 		{
@@ -27,7 +60,7 @@ module.exports = {
 			next_field_name: "chapter4_24",
 		},
 		{
-			no: ++I,
+			no: "merry",
 			type: CONSTANT.ANIME_IMAGE_TYPE,
 			name: "メリー",
 			loop: true,
@@ -46,7 +79,7 @@ module.exports = {
 			sound_name: null,
 		},
 		{
-			no: ++I,
+			no: "renko",
 			type: CONSTANT.ANIME_IMAGE_TYPE,
 			name: "蓮子",
 			loop: true,
