89d67523d8dd44542078d25380681e1f527f4b9f
sairoutine <sairoutine@gmail.com>
2019-02-25 02:29:57 +0900

    add: chapter4 serifs

--- src/js/data/field/chapter4/20.js
+++ src/js/data/field/chapter4/20.js
@@ -16,6 +16,35 @@ module.exports = {
 	background: "chapter4-20-bg-001",
 	walkSound: "walking_bare_default",
 	initialProcess: [
+		{"type": "criteria", "value": "existsItem", "arguments": ["27"], "process": [
+			// テープ「お父さんお母さんへ」を持っている
+			[
+				{"type": "criteria", "value": "isPlayed", "arguments": ["chapter4_20_40800"], "process": [
+					// すでにアクセス済み
+					[],
+					// まだ未アクセス
+					[
+						{"type": "process", "value": "playTalk", "arguments": [
+							[
+								{"chara": "renko", "exp": "renko-reaction_01", "serif":_("もう東洋哲学はいいわ")},
+								{"chara": "renko", "exp": null, "serif":_("認知の外側について考えるのは人に任せるわ")},
+								{"chara": "renko", "exp": "renko-reaction_04", "serif":_("私プラグマティストなの")},
+								{"chara": "renko", "exp": "renko-reaction_03", "serif":_("確認しようのない秘封をもとめるなんてナンセンスよ")},
+								{"chara": "merry", "exp": "merry-reaction_12", "serif":_("蓮子、もうやめて")},
+								{"chara": "renko", "exp": "renko-reaction_01", "serif":_("理解に苦しむわ、哲学を語るくせにこいつカントを読んでないのよ")},
+								{"chara": "merry", "exp": "merry-reaction_05", "serif":_("地蔵がしゃべっているのよ")},
+								{"chara": "merry", "exp": "merry-reaction_03", "serif":_("あなたカントにはあったことないでしょ")},
+								{"chara": "merry", "exp": "merry-reaction_05", "serif":_("でも地蔵はほら、いるのよ")},
+							]
+						]},
+						{"type": "process", "value": "incrementPlayedFlag", "arguments": ["chapter4_20_40800"]}
+					],
+				]},
+			],
+			// テープ「お父さんお母さんへ」を持っていない
+			[
+			]
+		]},
 	],
 	objects: [
 		{
@@ -43,26 +72,48 @@ module.exports = {
 			type: CONSTANT.ANIME_IMAGE_TYPE,
 			name: "地蔵",
 			serif: [
-				{"chara": "koishi", "exp": "look_front", "serif":_("なにこれ")},
-				{"chara": I, "serif":_("おや")},
-				{"chara": I, "serif":_("あなたは？")},
-				{"chara": "koishi", "exp": "look_front", "serif":_("わたし？")},
-				{"chara": "koishi", "exp": "yes", "serif":_("わたしはこいし")},
-				{"chara": I, "serif":_("こいし......")},
-				{"chara": I, "serif":_("あなたは何処へ向かっているのですか")},
-				{"chara": "koishi", "exp": "look_front", "serif":_("この先")},
-				{"chara": "chapter4-20-obj-02", "exp": "chapter4-20-obj-02-obj01", "serif":_("いいえ")},
-				{"chara": I, "serif":_("あなたは何を為そうとしているのですか")},
-				{"chara": "koishi", "exp": "look_front", "serif":_("なに？")},
-				{"chara": I, "serif":_("私はこの先の「虚無」に人が迷い込まぬよう見守っています")},
-				{"chara": I, "serif":_("知恵持たずして生きるは磁針無くして海漕ぎ出づるに同じ")},
-				{"chara": "renko", "serif":_("げっ")},
-				{"chara": "renko", "serif":_("いま地蔵がしゃべった？")},
-				{"chara": "renko", "serif":_("いよいよ奇怪ね")},
-				{"chara": "renko", "serif":_("いまの意味わかった？")},
-				{"chara": "renko", "serif":_("とりあえずあたりを調べなきゃね")},
-				{"chara": "renko", "serif":_("......")},
-				{"chara": "renko", "serif":_("喋り続けないと静けさで狂いそうだわ")},
+				 {"type": "criteria_serif", "criteria": "isPlayed", "arguments": ["chapter4_20_40800"], "serifs": [
+					// テープ「お父さんお母さんへ」を持っているときのセリフ再生済
+					[
+						{"chara": "renko", "exp": "renko-reaction_01", "serif":_("でたわね")},
+						{"chara": "renko", "exp": null, "serif":_("どうしたの")},
+						{"chara": "koishi", "exp": "wait", "serif":_("......")},
+						{"chara": "renko", "exp": "renko-reaction_04", "serif":_("この地蔵が通してくれないのよ")},
+						{"chara": "merry", "exp": "merry-reaction_10", "serif":_("貴方を待ってるみたいよ")},
+						{"chara": "chapter4-20-obj-01", "exp": "chapter4-20-obj-01-obj04", "serif":_("えい")},
+						{"chara": "merry", "exp": "merry-reaction_05", "serif":_("ちょっと！")},
+						{"chara": "koishi", "exp": "look_top", "serif":_("なあに")},
+						{"chara": "merry", "exp": "merry-reaction_10", "serif":_("いえ")},
+						{"chara": "merry", "exp": "merry-reaction_08", "serif":_("その")},
+						{"chara": "merry", "exp": "merry-reaction_09", "serif":_("なんでもないわ")},
+						{"chara": "renko", "exp": "renko-reaction_04", "serif":_("私だってこんなことしないわ")},
+						{"chara": "merry", "exp": "merry-reaction_09", "serif":_("やりかねないわ")},
+						{"chara": "renko", "exp": "renko-reaction_03", "serif":_("なにも起きなければいいんだけれど"),  "option": {"playEvent": "chapter4-20-event-01"}}, // 40820
+					],
+					// テープ「お父さんお母さんへ」を持っているときのセリフ未再生
+					[
+						{"chara": "koishi", "exp": "look_front", "serif":_("なにこれ")},
+						{"chara": I, "serif":_("おや")},
+						{"chara": I, "serif":_("あなたは？")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("わたし？")},
+						{"chara": "koishi", "exp": "yes", "serif":_("わたしはこいし")},
+						{"chara": I, "serif":_("こいし......")},
+						{"chara": I, "serif":_("あなたは何処へ向かっているのですか")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("この先")},
+						{"chara": "chapter4-20-obj-02", "exp": "chapter4-20-obj-02-obj01", "serif":_("いいえ")},
+						{"chara": I, "serif":_("あなたは何を為そうとしているのですか")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("なに？")},
+						{"chara": I, "serif":_("私はこの先の「虚無」に人が迷い込まぬよう見守っています")},
+						{"chara": I, "serif":_("知恵持たずして生きるは磁針無くして海漕ぎ出づるに同じ")},
+						{"chara": "renko", "serif":_("げっ")},
+						{"chara": "renko", "serif":_("いま地蔵がしゃべった？")},
+						{"chara": "renko", "serif":_("いよいよ奇怪ね")},
+						{"chara": "renko", "serif":_("いまの意味わかった？")},
+						{"chara": "renko", "serif":_("とりあえずあたりを調べなきゃね")},
+						{"chara": "renko", "serif":_("......")},
+						{"chara": "renko", "serif":_("喋り続けないと静けさで狂いそうだわ")},
+					],
+				]}
 			],
 			width: 100,
 			height: 100,
--- src/js/data/field/chapter4/42.js
+++ src/js/data/field/chapter4/42.js
@@ -15,6 +15,49 @@ module.exports = {
 	background: "chapter4-42-bg-001",
 	walkSound: "walking_bare_default",
 	initialProcess: [
+		{"type": "criteria", "value": "isPlayed", "arguments": ["chapter4_42_40830"], "process": [
+			// すでにアクセス済み
+			[],
+			// まだ未アクセス
+			[
+				{"type": "process", "value": "playTalk", "arguments": [
+					[
+						{"chara": "renko", "exp": "renko-reaction_05", "serif":_("なるほど")},
+						{"chara": "renko", "exp": "renko-reaction_01", "serif":_("確かにこの辺りは怪しいわ")},
+						{"chara": "merry", "exp": "merry-reaction_08", "serif":_("境界線が見つかりそう")},
+						{"chara": "merry", "exp": "merry-reaction_10", "serif":_("でもメビウス環的パラドックスの気配もするわ")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("私にまかせて！")},
+						{"chara": "koishi", "exp": null, "serif":_("何を探せばいいんだっけ")},
+						{"chara": "merry", "exp": "merry-reaction_03", "serif":_("......")},
+						{"chara": "merry", "exp": "merry-reaction_09", "serif":_("境界を探すのよ")},
+						{"chara": "merry", "exp": "merry-reaction_06", "serif":_("私はそれを見ればそれとわかる")},
+						{"chara": "renko", "exp": "renko-reaction_02", "serif":_("ここに来た時もそこを通じて来たからこのあたりのはずよ")},
+						{"chara": "merry", "exp": "merry-reaction_07", "serif":_("私はその気配を感じ取れる")},
+						{"chara": "renko", "exp": "renko-reaction_06", "serif":_("私は緯度と経度がわかる")},
+						{"chara": "koishi", "exp": "look_top", "serif":_("けーどって何")},
+						{"chara": "koishi", "exp": null, "serif":_("......")},
+						{"chara": "renko", "exp": "renko-reaction_05", "serif":_("二酸化ケイ素が土の状態を取るとき――")},
+						{"chara": "merry", "exp": "merry-reaction_10", "serif":_("やめなさいよ")},
+						{"chara": "merry", "exp": "merry-reaction_07", "serif":_("蓮子は今自分がどこにいるかわかるのよ")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("すごーい")},
+						{"chara": "koishi", "exp": null, "serif":_("でも私に任せて")},
+						{"chara": "koishi", "exp": "yes", "serif":_("私が二人をお家に返してあげる！")},
+						{"chara": "koishi", "exp": "3rdeye", "serif":_("私は専門家だから！")},
+						{"chara": "renko", "exp": "renko-reaction_01", "serif":_("なんの？")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("ともだち作ったり仲直りさせたり")},
+						{"chara": "koishi", "exp": null, "serif":_("雪だるまだって名人よ！")},
+						{"chara": "koishi", "exp": "yes", "serif":_("クッキーだって作れる")},
+						{"chara": "merry", "exp": "merry-reaction_05", "serif":_("ねえ")},
+						{"chara": "merry", "exp": "merry-reaction_10", "serif":_("一回だけチャンスをあげましょ")},
+						{"chara": "renko", "exp": "renko-reaction_04", "serif":_("あのね、あなた帰りたいんだったら”自分の足で歩く”のよ")},
+						{"chara": "merry", "exp": "merry-reaction_10", "serif":_("それは彼女も一緒でしょ")},
+						{"chara": "koishi", "exp": "look_front", "serif":_("はやくはじめましょ！")},
+					]
+				]},
+				{"type": "process", "value": "incrementPlayedFlag", "arguments": ["chapter4_42_40830"]}
+			],
+		]},
+
 	],
 	objects: [
 		{
