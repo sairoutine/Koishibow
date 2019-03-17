4f92d0286b4daf09f06ec1541f54a1203e024d11
sairoutine <sairoutine@gmail.com>
2019-03-17 21:15:17 +0900

    add: event chapter4-19-event-01

--- src/js/data/event/chapter4/19/01.js
+++ src/js/data/event/chapter4/19/01.js
@@ -7,8 +7,15 @@ module.exports = {
 		"chapter4-19-event-01-obj02",
 	],
 	serifs: [
-		{"exp": "chapter4-19-event-01-obj01", "serif": "", "option": {"x": 5000, "y": 2500, "loop": false}},
-		{"exp": "chapter4-19-event-01-obj02", "serif": "", "option": {"x": 5000, "y": 2500, "loop": false}},
+		{"exp": "chapter4-19-event-01-obj01", "serif": "", "option": {"x": 5000, "y": 2500, "loop": false, "sounds": [
+			{"name": "chapter4-19-event-01-01", "frame": 0},
+		]}},
+		{"exp": "chapter4-19-event-01-obj02", "serif": "", "option": {"x": 5000, "y": 2500, "loop": false, "sounds": [
+			{"name": "chapter4-19-event-01-02", "frame": 0},
+			{"name": "chapter4-19-event-01-03", "frame": 150},
+			{"name": "chapter4-19-event-01-04", "frame": 320},
+			{"name": "chapter4-19-event-01-05", "frame": 380},
+		]}},
 	],
 	endAnime: null,
 	endSounds: [
