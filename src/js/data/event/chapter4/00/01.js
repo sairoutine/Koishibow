4d73b039c5c3ed6320cd1863b883dec241cce075
sairoutine <sairoutine@gmail.com>
2019-03-17 19:20:16 +0900

    add: chapter4-00-event-01 sound

--- src/js/data/event/chapter4/00/01.js
+++ src/js/data/event/chapter4/00/01.js
@@ -17,13 +17,22 @@ module.exports = {
 		"chapter4-00-event-01-obj11",
 	],
 	serifs: [
-		{"exp": "chapter4-00-event-01-obj01", "serif": _("ふわー"), "option": {"x": 325, "y": 125, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj01", "serif": _("ふわー"), "option": {"x": 325, "y": 125, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-01", "frame": 10},
+		]}},
 		{"exp": null, "serif": _("おはよう"), "option": {"x": 325, "y": 125, "loop": false}},
 		{"exp": null, "serif": _("あれ？"), "option": {"x": 325, "y": 125, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj02", "serif": _("うーん"), "option": {"x": 376, "y": 176, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj03", "serif": _("おめめがいてて"), "option": {"x": 376, "y": 176, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj02", "serif": _("うーん"), "option": {"x": 376, "y": 176, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-02", "frame": 30},
+		]}},
+		{"exp": "chapter4-00-event-01-obj03", "serif": _("おめめがいてて"), "option": {"x": 376, "y": 176, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-03", "frame": 10},
+			{"name": "chapter4-00-event-01-04", "frame": 60},
+		]}},
 		{"exp": null, "serif": _("どうしちゃったんだろう"), "option": {"x": 376, "y": 176, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj04", "serif": _("まあ！"), "option": {"x": 700, "y": 200, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj04", "serif": _("まあ！"), "option": {"x": 700, "y": 200, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-05", "frame": 25},
+		]}},
 		{"exp": "chapter4-00-event-01-obj05", "serif": _("目が覚めたのねえ！"), "option": {"x": 480, "y": 200, "loop": false}},
 		{"exp": null, "serif": _("え？"), "option": {"x": 376, "y": 176, "loop": false}},
 		{"exp": null, "serif": _("おねえちゃん！"), "option": {"x": 376, "y": 176, "loop": false}},
@@ -38,23 +47,35 @@ module.exports = {
 		{"exp": null, "serif": _("……"), "option": {"x": 480, "y": 200, "loop": false}},
 		{"exp": null, "serif": _("わからないわ"), "option": {"x": 480, "y": 200, "loop": false}},
 		{"exp": "chapter4-00-event-01-obj07", "serif": _("夢でも見てたんじゃないの"), "option": {"x": 480, "y": 200, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj08", "serif": _("夢じゃないよ！"), "option": {"x": 376, "y": 176, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj08", "serif": _("夢じゃないよ！"), "option": {"x": 376, "y": 176, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-06", "frame": 20},
+		]}},
 		{"exp": null, "serif": _("でもお名前もわからないんでしょう？"), "option": {"x": 480, "y": 200, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj08", "serif": _("わかるよ！"), "option": {"x": 376, "y": 176, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj08", "serif": _("わかるよ！"), "option": {"x": 376, "y": 176, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-06", "frame": 20},
+		]}},
 		{"exp": null, "serif": _("それじゃあ言ってごらんなさい"), "option": {"x": 480, "y": 200, "loop": false}},
 		{"exp": null, "serif": _("......"), "option": {"x": 376, "y": 176, "loop": false}},
 		{"exp": null, "serif": _("ほら言えないでしょう"), "option": {"x": 480, "y": 200, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj08", "serif": _("もういい！"), "option": {"x": 376, "y": 176, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj08", "serif": _("もういい！"), "option": {"x": 376, "y": 176, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-06", "frame": 20},
+		]}},
 		{"exp": null, "serif": _("お友達に会いに行くわ！"), "option": {"x": 376, "y": 176, "loop": false}},
 		{"exp": null, "serif": _("お友達？"), "option": {"x": 480, "y": 200, "loop": false}},
 		{"exp": null, "serif": _("お友達なんていないでしょう、あなた"), "option": {"x": 480, "y": 200, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj08", "serif": _("いるもん"), "option": {"x": 376, "y": 176, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj08", "serif": _("友達いるもん！"), "option": {"x": 376, "y": 176, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj08", "serif": _("いるもん"), "option": {"x": 376, "y": 176, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-06", "frame": 20},
+		]}},
+		{"exp": "chapter4-00-event-01-obj08", "serif": _("友達いるもん！"), "option": {"x": 376, "y": 176, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-06", "frame": 20},
+		]}},
 		{"exp": "chapter4-00-event-01-obj09", "serif": _("あなたに友達なんかいるわけないでしょ"), "option": {"x": 480, "y": 200, "loop": false}},
 		{"exp": null, "serif": _("......"), "option": {"x": 376, "y": 176, "loop": false}},
 		{"exp": "chapter4-00-event-01-obj10", "serif": _("じゃあ外見て"), "option": {"x": 376, "y": 176, "loop": false}},
 		{"exp": null, "serif": _("はあ"), "option": {"x": 480, "y": 200, "loop": false}},
-		{"exp": "chapter4-00-event-01-obj11", "serif": _("それで？"), "option": {"x": 200, "y": 0, "loop": false}},
+		{"exp": "chapter4-00-event-01-obj11", "serif": _("それで？"), "option": {"x": 200, "y": 0, "loop": false, "sounds": [
+			{"name": "chapter4-00-event-01-07", "frame": 90},
+		]}},
 		{"exp": null, "serif": _("何も見えないわ"), "option": {"x": 200, "y": 0, "loop": false}},
 		{"exp": null, "serif": _("ほらそこよ"), "option": {"x": 376, "y": 176, "loop": false}},
 		{"exp": null, "serif": _("そこのずーっと下よ！"), "option": {"x": 376, "y": 176, "loop": false}},
