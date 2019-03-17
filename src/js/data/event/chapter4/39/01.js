6b23db69fea0e21bb1855e55267581e2309f302e
sairoutine <sairoutine@gmail.com>
2019-03-17 20:41:24 +0900

    add: event chapter4-39-event-01

--- src/js/data/event/chapter4/39/01.js
+++ src/js/data/event/chapter4/39/01.js
@@ -23,11 +23,14 @@ module.exports = {
 		{"exp": null, "serif": _("....."), "option": {"x": 370, "y": 210, "loop": false}},
 		{"exp": null, "serif": _("....."), "option": {"x": 370, "y": 210, "loop": false}},
 		{"exp": null, "serif": _("いく"), "option": {"x": 370, "y": 210, "loop": false}},
-		{"exp": "chapter4-39-event-01-obj04", "serif": _("こいし！"), "option": {"x": 730, "y": 285, "loop": false}},
+		{"exp": "chapter4-39-event-01-obj04", "serif": _("こいし！"), "option": {"x": 730, "y": 285, "loop": false, "sounds": [
+			{"name": "chapter4-39-event-01-01", "frame": 300},
+		]}},
 		{"exp": "chapter4-39-event-01-obj05", "serif": _("どこなの！"), "option": {"x": 730, "y": 285, "loop": false}},
 	],
 	endAnime: "chapter4-39-event-01-obj06",
 	endSounds: [
+		{"name": "chapter4-39-event-01-02", "frame": 130},
 	],
 	endProcess: [
 		// chapter5 へ
