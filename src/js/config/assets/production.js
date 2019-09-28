e4ad0043a9725143a91d43542d5e81af8e874a22
sairoutine <sairoutine@gmail.com>
2019-09-29 03:29:06 +0900

    fix: image path

--- src/js/config/assets/production.js
+++ src/js/config/assets/production.js
@@ -43,7 +43,7 @@ AssetsConfig.images = {
     
 	// 2-1 こいしが起きる
 	"chapter2-01-event-01-koishi":  "./image/production/chapter2/event/chapter2-01-event-01/chapter2-01-event-01-koishi.png",
-	"chapter2-01-event-01-obj-01":  "./image/production/chapter2/event/chapter2-01-event-01/chapter2-01-event-01-obj-01.jpg",	
+	"chapter2-01-event-01-obj-01":  "./image/production/chapter2/event/chapter2-01-event-01/chapter2-01-event-01-obj-01.png",	
 
 	// 3-1 こいしが起きる
 	"chapter3-01-event-01-koishi":  "./image/production/chapter3/event/chapter3-01-event-01/chapter3-01-event-01-koishi.png",
@@ -267,7 +267,7 @@ AssetsConfig.images = {
 	//
 
 	// chapter2
-	"chapter2-07-bg-001": "./image/production/chapter2/chapter2-07/chapter2-07-bg-001.jpg",
+	"chapter2-07-bg-001": "./image/production/chapter2/chapter2-07/chapter2-07-bg-001.png",
 	"chapter2-07-obj-01": "./image/production/chapter2/chapter2-07/chapter2-07-obj-01.png",
 
 	// chapter3
