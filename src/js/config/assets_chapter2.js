6a150299b7422f19c8f6a1d17f6c9e0734eb1a03
usamata <non1usamata@gmail.com>
2019-10-13 03:43:26 +0900

    chapter2-09関連追加

--- src/js/config/assets_chapter2.js
+++ src/js/config/assets_chapter2.js
@@ -13,6 +13,7 @@ AssetsConfig.images = {
 	"chapter2-07-bg-001": "./image/production/chapter2/chapter2-07/chapter2-07-bg-001.png",
 	"chapter2-07-obj-01": "./image/production/chapter2/chapter2-07/chapter2-07-obj-01.png",
 	"chapter2-08-bg-001": "./image/production/chapter2/chapter2-08/chapter2-08-bg-001.png",
+	"chapter2-09-bg-001": "./image/production/chapter2/chapter2-09/chapter2-09-bg-001.png",
 
 };
 
